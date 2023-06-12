565f845dbca4a779c1b0e9f8d879a648c26317b8
SuperWinds <215997128@qq.com>
2023-06-12 18:46:22 +0800

    Update bingdata_source.m

--- bingdata_source.m
+++ bingdata_source.m
@@ -8,7 +8,7 @@
     PAMovieClip *buy_btn = [__youfunction__MV getChildByName:@"buy_btn"];
     [buy_btn attachEventListener:ON_CLICK withTarget:self andSelector:@"buyPetEgg:withParams:" andParams:[NSString stringWithFormat:@"%@",eggid]];
     
-    //绑定
+    //绑定图片显示
     PASprite* aritfact_bg_Img = [self loadFlashArtifactSprite:[artifact_info_data objectForKey:@"image"] withSize:CGPointMake(100, 100)];
     [[self.mV getChildByName:@"artifact_img"] removeAllChildren];
     [[self.mV getChildByName:@"artifact_img"] addChild:aritfact_bg_Img withName:@"back_ground"  z:self.dynamicContentZLayer];
